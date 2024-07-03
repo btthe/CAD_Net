8904cd73676016a9dbc48843c2a76ca4747a61df
btthe <149663455+btthe@users.noreply.github.com>
2024-07-03 22:01:33 +0800

    Add files via upload

---
+++ config_training.py
@@ -0,0 +1,16 @@
+config = {#'stage1_data_path': '/work/DataBowl3/stage1/stage1/',
+          'luna_raw': 'D:/1TJU/AAcode/practice2/Luna/lunaset/raw',
+          'luna_segment': 'D:/1TJU\AAcode/practice2/Luna/lunaset/seg-lungs-LUNA16/',
+
+          'luna_data': 'D:/1TJU/AAcode/practice2/Luna/lunaset/allset',
+          'preprocess_result_path': 'D:/1TJU/AAcode/practice2/Luna/lunaset/preprocess/',
+          # 'preprocess_result_path': '/data/gongli/gldata/preprocess/',
+
+          'luna_abbr': 'D:/1TJU/AAcode/practice2/Luna/labels/shorter.csv',
+          'luna_label': 'D:/1TJU/AAcode/practice2/Luna/labels/lunaannos.csv',
+
+          'bbox_path_train': '../classifier/bboxsenet4/',
+          'bbox_path_value': '../classifier/bboxsenet4/',
+          'bbox_path_test': '../classifier/bboxsenet4/',
+          # 'preprocessing_backend': 'python'
+          }
